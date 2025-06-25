221c2653390b175f14e4ff82fd7a0fe466f6830a
North <1227379879@qq.com>
2025-06-25 23:51:25 +0800

    feat: 更新 @northal/crud-element-plus 版本至 1.0.2，添加 CSS 导入，优化依赖项

--- packages/playground/src/main.ts
+++ packages/playground/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import '@northal/crud-element-plus/index.css'
 
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
